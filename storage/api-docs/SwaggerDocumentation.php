6313bd40abf1d28a55af56304567240a7e8da6fb
Eliab Bernardino <eliab2107@gmail.com>
2025-01-29 13:33:29 -0300

    configure swagger and env.example

---
+++ storage/api-docs/SwaggerDocumentation.php
@@ -0,0 +1,14 @@
+<?php
+
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Info(
+ *     version="1.0.0",
+ *     title="Minha API",
+ *     description="Descrição da API",
+ *     @OA\Contact(
+ *         email="suporte@minhaapi.com"
+ *     )
+ * )
+ */
